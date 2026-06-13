233d7ee9d48ad1b16e66c687fcfa209fc71f34c8
t8rtot <t8rtot.tb@gmail.com>
2026-06-13 12:09:10 -0700

    feat(sim): add public barrel export; remove smoke test

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,7 @@
+export * from './math';
+export * from './types';
+export * from './constants';
+export * from './rng';
+export * from './state';
+export * from './step';
+export { movementSystem } from './systems/movement';
--- packages/shared/src/smoke.test.ts
+++
@@ -1,3 +0,0 @@
-test('toolchain runs', () => {
-  expect(1 + 1).toBe(2);
-});
